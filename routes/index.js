f50a4c74be5aaa26937abce37bbe61b48bab8edf
Xiu_Er <13813641925@163.com>
2020-11-20 00:50:25 +0800

    新增request 的获取参数api req.body req.query req.url

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. 首页路由 */
-router.get('/', function(req, res, next) {
+router.get('/index?:id', function(req, res, next) {
+  console.log(req.params) //只适合于restfull风格 例子 ： /index/:id
   console.log('这里是中间件');
+  console.log(req.url)
+  //query 只能获取get请求参数，其他的post, put请求不能获取
+  console.log(req.query)
+  console.log(req.query.id)
   //继续下一个中间件
   next();
 },function (req,res,next) {
@@ -23,6 +28,14 @@ router.get('/', function(req, res, next) {
       })
 });
 
+/**
+ * restful风格
+ */
+router.get('/index/:id', function(req, res, next) {
+  console.log(req.params)
+  res.send((req.params))
+})
+
 // 定义一个GET请求"/world" 的路由，执行方法
 router.get('/getWorld',function (req,res,next) {
   res.render('index',{title: 'Hello world！ get'})
@@ -30,7 +43,11 @@ router.get('/getWorld',function (req,res,next) {
 
 // Post请求方式
 router.post('/postWorld',function (req,res,next) {
-  res.render('index',{title : 'Hello world! post'});
+  console.log(req.params)
+  console.log(req.body)
+  console.log(req.body.id)  //在返回前可以去直接解析json
+ // res.render('index',{title : 'Hello world! post'});
+  res.send(req.body)  //此我地方不可以直接解析json key
 })
 
 // Put请求方式
